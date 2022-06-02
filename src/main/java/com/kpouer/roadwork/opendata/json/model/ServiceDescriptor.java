84a1578e2bec3415ce15ca60a5d9cacad387e872
Matthieu Casanova <kpouer@users.sourceforge.net>
2022-06-02 07:41:12 +0200

    add more attributes and add licence

--- src/main/java/com/kpouer/roadwork/opendata/json/model/ServiceDescriptor.java
+++ src/main/java/com/kpouer/roadwork/opendata/json/model/ServiceDescriptor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 Matthieu Casanova
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.kpouer.roadwork.opendata.json.model;
 
 import com.kpouer.mapview.LatLng;
@@ -7,8 +22,12 @@ import java.util.Locale;
 public class ServiceDescriptor {
     private String country;
     private double[] center;
+    private String source_url;
     private String url;
     private String name;
+    private String producer;
+    private String licenceName;
+    private String licenceUrl;
     private String id;
     private String latitude;
     private String longitude;
@@ -35,6 +54,38 @@ public class ServiceDescriptor {
 
     public void setCenter(double[] center) {
         this.center = center;
+    }
+
+    public String getSource_url() {
+        return source_url;
+    }
+
+    public void setSource_url(String source_url) {
+        this.source_url = source_url;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getLicenceName() {
+        return licenceName;
+    }
+
+    public void setLicenceName(String licenceName) {
+        this.licenceName = licenceName;
+    }
+
+    public String getLicenceUrl() {
+        return licenceUrl;
+    }
+
+    public void setLicenceUrl(String licenceUrl) {
+        this.licenceUrl = licenceUrl;
     }
 
     public String getUrl() {
