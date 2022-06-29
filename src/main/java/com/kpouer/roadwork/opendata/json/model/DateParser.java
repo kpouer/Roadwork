14ffc36bb551201466bfacd14d6d9849902fe04a
Matthieu Casanova <kpouer@users.sourceforge.net>
2022-06-29 21:00:00 +0200

    remove useless import

--- src/main/java/com/kpouer/roadwork/opendata/json/model/DateParser.java
+++ src/main/java/com/kpouer/roadwork/opendata/json/model/DateParser.java
@@ -19,7 +19,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
