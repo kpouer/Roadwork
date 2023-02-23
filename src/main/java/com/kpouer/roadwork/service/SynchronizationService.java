62dfcce7bb1d0e4832f13a85e22675ff51404611
Matthieu Casanova <public@kpouer.com>
2023-02-23 13:55:07 +0100

    fix the case where we don't get synchronize data

--- src/main/java/com/kpouer/roadwork/service/SynchronizationService.java
+++ src/main/java/com/kpouer/roadwork/service/SynchronizationService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Matthieu Casanova
+ * Copyright 2022-2023 Matthieu Casanova
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,20 +22,17 @@ import com.kpouer.roadwork.event.SynchronizationSettingsUpdated;
 import com.kpouer.roadwork.model.Roadwork;
 import com.kpouer.roadwork.model.RoadworkData;
 import com.kpouer.roadwork.model.sync.SyncData;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 import javax.swing.*;
@@ -49,9 +46,8 @@ import java.util.Map;
  * @author Matthieu Casanova
  */
 @Service
+@Slf4j
 public class SynchronizationService {
-    private static final Logger logger = LoggerFactory.getLogger(SynchronizationService.class);
-
     private final UserSettings userSettings;
     private final LocalizationService localizationService;
     private final SoftwareModel softwareModel;
@@ -77,28 +73,30 @@ public class SynchronizationService {
      */
     public void synchronize(RoadworkData roadworkData) {
         if (userSettings.isSynchronizationEnabled()) {
-            logger.info("synchronize");
-            String url = getUrl(roadworkData.getSource());
-            logger.info("Will synchronize with url {}", url);
-            RestOperations restTemplate = new RestTemplate();
+            log.info("synchronize");
+            var url = getUrl(roadworkData.getSource());
+            log.info("Will synchronize with url {}", url);
+            var restTemplate = new RestTemplate();
             try {
-                Map<String, SyncData> body = new HashMap<>();
-                for (Roadwork roadwork : roadworkData) {
+                var body = new HashMap<String, SyncData>();
+                for (var roadwork : roadworkData) {
                     body.put(roadwork.getId(), roadwork.getSyncData());
                 }
-                ParameterizedTypeReference<Map<String, SyncData>> responseType = new ParameterizedTypeReference<>() {
-                };
-                HttpEntity<Map<String, SyncData>> entity = new HttpEntity<>(body, createHeaders());
-                ResponseEntity<Map<String, SyncData>> roadworkDataResponseEntity = restTemplate.exchange(url, HttpMethod.POST, entity, responseType);
-                Map<String, SyncData> synchronizedData = roadworkDataResponseEntity.getBody();
-                for (Map.Entry<String, SyncData> entry : synchronizedData.entrySet()) {
-                    SyncData serverSyncData = entry.getValue();
-                    Roadwork roadwork = roadworkData.getRoadwork(entry.getKey());
+                var responseType = new MapParameterizedTypeReference();
+                var entity = new HttpEntity<Map<String, SyncData>>(body, createHeaders());
+                var roadworkDataResponseEntity = restTemplate.exchange(url, HttpMethod.POST, entity, responseType);
+                var synchronizedData = roadworkDataResponseEntity.getBody();
+                if (synchronizedData == null) {
+                    throw new RestClientException("No body in response");
+                }
+                for (var entry : synchronizedData.entrySet()) {
+                    var serverSyncData = entry.getValue();
+                    var roadwork = roadworkData.getRoadwork(entry.getKey());
                     roadwork.getSyncData().copy(serverSyncData);
                     roadwork.updateMarker();
                 }
             } catch (HttpClientErrorException.Unauthorized e) {
-                logger.warn("Error posting to synchronization server, invalid credencials");
+                log.warn("Error posting to synchronization server, invalid credencials");
                 JOptionPane.showMessageDialog(softwareModel.getMainFrame(),
                         localizationService.getMessage("dialog.synchronization.unauthorized.message"),
                         localizationService.getMessage("dialog.synchronization.unauthorized.title"),
@@ -106,11 +104,11 @@ public class SynchronizationService {
                 applicationContext.getBean(SynchronizationSettingsAction.class).actionPerformed(null);
             } catch (RestClientException e) {
                 if (e.getCause() instanceof ConnectException) {
-                    logger.error("Unable to connect to synchronization server " + url);
+                    log.error("Unable to connect to synchronization server {}", url);
                     userSettings.setSynchronizationEnabled(false);
                     applicationEventPublisher.publishEvent(new SynchronizationSettingsUpdated(this));
                 } else {
-                    logger.error("Error posting to synchronization server", e);
+                    log.error("Error posting to synchronization server", e);
                 }
             }
         }
@@ -118,8 +116,8 @@ public class SynchronizationService {
 
     @NotNull
     private String getUrl(String source) {
-        String synchronizationTeam = userSettings.getSynchronizationTeam();
-        String url = userSettings.getSynchronizationUrl();
+        var synchronizationTeam = userSettings.getSynchronizationTeam();
+        var url = userSettings.getSynchronizationUrl();
         if (!url.endsWith("/")) {
             url += '/';
         }
@@ -128,11 +126,14 @@ public class SynchronizationService {
     }
 
     private HttpHeaders createHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        String auth = userSettings.getSynchronizationLogin() + ':' + userSettings.getSynchronizationPassword();
-        String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.US_ASCII));
-        String authHeader = "Basic " + encodedAuth;
+        var headers = new HttpHeaders();
+        var auth = userSettings.getSynchronizationLogin() + ':' + userSettings.getSynchronizationPassword();
+        var encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.US_ASCII));
+        var authHeader = "Basic " + encodedAuth;
         headers.set("Authorization", authHeader);
         return headers;
+    }
+
+    private static class MapParameterizedTypeReference extends ParameterizedTypeReference<Map<String, SyncData>> {
     }
 }
