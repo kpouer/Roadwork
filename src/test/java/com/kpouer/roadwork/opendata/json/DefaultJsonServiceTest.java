6477c83fc466527f35af781ee258b62c53f08d57
Matthieu Casanova <public@kpouer.com>
2023-03-04 17:40:54 +0100

    fix tests

--- src/test/java/com/kpouer/roadwork/opendata/json/DefaultJsonServiceTest.java
+++ src/test/java/com/kpouer/roadwork/opendata/json/DefaultJsonServiceTest.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
-import com.kpouer.roadwork.model.Roadwork;
-import com.kpouer.roadwork.model.RoadworkData;
 import com.kpouer.roadwork.opendata.json.model.ServiceDescriptor;
 import com.kpouer.roadwork.service.HttpService;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,13 +16,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,9 +37,8 @@ class DefaultJsonServiceTest {
 
     @Test
     void test() throws IOException {
-
-        try (InputStream stream = DefaultJsonServiceTest.class.getResourceAsStream("/sample/france/montpellier.json")) {
-            Object document = Configuration.defaultConfiguration().jsonProvider().parse(stream, "UTF-8");
+        try (var stream = DefaultJsonServiceTest.class.getResourceAsStream("/sample/france/montpellier.json")) {
+            var document = Configuration.defaultConfiguration().jsonProvider().parse(stream, "UTF-8");
             List<Object> read = JsonPath.read(document, "$.features");
             System.out.println();
         }
@@ -50,19 +46,26 @@ class DefaultJsonServiceTest {
 
     @Test
     void testAvignon() throws IOException, URISyntaxException {
-        DefaultJsonService serviceDescriptor = getServiceDescriptor("json/France-Avignon.json", "/sample/france/avignon.json");
-        Optional<RoadworkData> data = serviceDescriptor.getData();
-        RoadworkData roadworkData = data.get();
-        Roadwork roadwork = roadworkData.getRoadworks().get("850041398");
+        var serviceDescriptor = getServiceDescriptor("opendata/json/France-Avignon.json", "/sample/france/avignon.json");
+        var data = serviceDescriptor.getData();
+        var roadworkData = data.get();
+        var roadwork = roadworkData.getRoadworks().get("850041398");
         assertEquals("850041398", roadwork.getId());
     }
 
     private DefaultJsonService getServiceDescriptor(String pathname, String samplePath) throws IOException, URISyntaxException {
-        String sample = Files.readString(Paths.get(getClass().getResource(samplePath).toURI()), StandardCharsets.UTF_8);
-        Mockito.when(httpService.getUrl(ArgumentCaptor.forClass(String.class).capture())).thenReturn(sample);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        ServiceDescriptor serviceDescriptor = objectMapper.readValue(new File(pathname), ServiceDescriptor.class);
+        Mockito.when(httpService.getUrl(ArgumentCaptor.forClass(String.class).capture())).thenReturn(getSample(samplePath));
+        var objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        var file = new File(pathname);
+        var serviceDescriptor = objectMapper.readValue(file, ServiceDescriptor.class);
         return new DefaultJsonService(pathname, httpService, serviceDescriptor);
+    }
+
+    private String getSample(String samplePath) throws URISyntaxException, IOException {
+        var uri = getClass().getResource(samplePath).toURI();
+        var path = Paths.get(uri);
+        var sample = Files.readString(path, StandardCharsets.UTF_8);
+        return sample;
     }
 }
